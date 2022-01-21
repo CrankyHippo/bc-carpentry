5a1eeb9e30b82f1c8d68af72b391e393b4e5b5eb
Bryan Burnett <crankyhippo@gmail.com>
2022-01-21 13:04:05 -0800

    css update

--- src/button/Button.tsx
+++ src/button/Button.tsx
@@ -20,7 +20,7 @@ const Button = (props: IButtonProps) => {
       <style jsx>
         {`
           .btn {
-            @apply inline-block rounded-md text-center;
+            @apply inline-block rounded-md text-center border-2 border-white;
           }
 
           .btn-base {
