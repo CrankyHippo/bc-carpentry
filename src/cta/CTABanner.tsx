c024e5b4b9fbc05f0e3bd6a510594cd9f0150980
Bryan Burnett <crankyhippo@gmail.com>
2022-01-21 13:29:34 -0800

    Added img to component

--- src/cta/CTABanner.tsx
+++ src/cta/CTABanner.tsx
@@ -1,8 +1,12 @@
 import { ReactNode } from 'react';
 
+import router from 'next/router';
+
 type ICTABannerProps = {
   title: string;
   subtitle: string;
+  image: string;
+  imageAlt: string;
   button: ReactNode;
 };
 
@@ -11,6 +15,14 @@ const CTABanner = (props: ICTABannerProps) => (
     <div className="text-2xl font-semibold">
       <div className="text-gray-900">{props.title}</div>
       <div className="text-primary-500">{props.subtitle}</div>
+    </div>
+
+    <div className="w-full p-4 border-4 border-solid shadow-2xl sm:w-1/2 border-sky-500">
+      <img
+        className="rounded-3xl"
+        src={`${router.basePath}${props.image}`}
+        alt={props.imageAlt}
+      />
     </div>
 
     <div className="mt-3 whitespace-no-wrap sm:mt-0 sm:ml-2">
