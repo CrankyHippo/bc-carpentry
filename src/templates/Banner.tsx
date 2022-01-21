5afb3186b3a8068cc4c918feaab1492e5149a21a
Bryan Burnett <crankyhippo@gmail.com>
2022-01-21 13:29:55 -0800

    img / imageAlt update

--- src/templates/Banner.tsx
+++ src/templates/Banner.tsx
@@ -9,6 +9,8 @@ const Banner = () => (
     <CTABanner
       title="Follow us on Instagram to see our latest work!"
       subtitle=""
+      image="/assets/images/instagram_slide.png"
+      imageAlt="Instagram photos"
       button={
         <Link href="https://www.instagram.com/brentccarpentry/">
           <a>
