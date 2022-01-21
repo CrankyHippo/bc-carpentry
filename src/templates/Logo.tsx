45ca4dc70453cf54b02dec17f8cfc929d56456f1
Bryan Burnett <crankyhippo@gmail.com>
2022-01-21 13:03:46 -0800

    css changes

--- src/templates/Logo.tsx
+++ src/templates/Logo.tsx
@@ -6,12 +6,12 @@ type ILogoProps = {
 
 const Logo = (props: ILogoProps) => {
   const fontStyle = props.xl
-    ? 'font-semibold text-3xl'
+    ? 'font-semibold text-2xl'
     : 'font-semibold text-xl';
 
   return (
     <span
-      className={`text-gray-900 inline-flex items-center rounded-3xl ${fontStyle}`}
+      className={`text-white bg-stone-500 border-2 border-white inline-flex items-center rounded-xl font-extrabold text-xl py-4 px-6 ${fontStyle}`}
     >
       {AppConfig.site_name}
     </span>
